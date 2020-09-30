34e095522728dd0205742b2e699f7a8840e90196
Andromeda <duramaxbuild01@gmail.com>
2020-09-30 00:49:57 -0400

    feature added, for future use

--- config.js
+++ config.js
@@ -1,4 +1,5 @@
 const chalk = require('chalk');
+const test = require('./commands/test');
 var sqlite3 = require('sqlite3').verbose();
 
 
@@ -7,9 +8,10 @@ var db = new sqlite3.Database('./db/user.db', (err) => {
         return console.error(err.message, err.stack)
     }
     db.run("CREATE TABLE if not exists bannedUser (user TEXT, reason TEXT)");
+    db.run("CREATE TABLE if not exists warnedUsers (user TEXT, reason TEXT)");
+    //db.run("DROP TABLE warnedUsers")
     console.log(chalk.greenBright(`Connected to Database`));
 })
-
 
 
 module.exports = db;
