a58dbae6b75828e366ea1112c0a4657823be86fc
Andromeda <duramaxbuild01@gmail.com>
2020-10-01 17:42:09 -0400

    audit uses channel names

--- commands/updates.js
+++ commands/updates.js
@@ -6,24 +6,24 @@ const XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest;
 let http = new XMLHttpRequest();
 
 module.exports = {
-	name: 'updates',
-	execute(message) {
-		fs.readFile('updates.md', function(err, data) {
-			if (err) return console.log(err);
-			http = new XMLHttpRequest();
-			http.open(
-				'GET',
-				`${url}`,
-				true,
-			);
-			http.send();
-			text = http.responseText;
-			const helpEmbed = new mess.MessageEmbed()
-				.setColor(0x28c9d0)
-				.setTitle('Updates and Issues!')
-				.setDescription(data.toString())
-				.setFooter('Legacy Audit: Updates');
-			message.channel.send(helpEmbed);
-		});
-	},
+    name: 'updates',
+    execute(message) {
+        fs.readFile('updates.md', function(err, data) {
+            if (err) return console.log(err);
+            http = new XMLHttpRequest();
+            http.open(
+                'GET',
+                `${url}`,
+                true,
+            );
+            http.send();
+            text = http.responseText;
+            const updateEmbed = new mess.MessageEmbed()
+                .setColor(0x28c9d0)
+                .setTitle('Updates and Issues!')
+                .setDescription(data.toString())
+                .setFooter('Legacy Audit: Updates');
+            message.channel.send(updateEmbed);
+        });
+    },
 };
--- events/audit.Log.js
+++ events/audit.Log.js
@@ -1,39 +1,38 @@
 const mess = require("discord.js");
+const { welcome } = require("../prop.json");
 
-module.exports = [
-  {
-    name: "guildMemberAdd",
-    execute(member) {
-      const helpEmbed = new mess.MessageEmbed()
+module.exports = [{
+        name: "guildMemberAdd",
+        execute(member, message) {
+            const helpEmbed = new mess.MessageEmbed()
 
-        .setColor(0x28c9d0)
+            .setColor(0x28c9d0)
 
-        .setTitle(`${member.user.tag} (${member.id})`)
+            .setTitle(`${member.user.tag} (${member.id})`)
 
-        .setDescription("Joined the guild!")
+            .setDescription("Joined the guild!")
 
-        .setFooter("Legacy Audit: Joined")
+            .setFooter("Legacy Audit: Joined")
 
-        .setTimestamp();
-      member.guild.channels.cache.get("743231991897522256").send(helpEmbed);
+            .setTimestamp();
+            member.guild.channels.cache.find(channel => channel.name === welcome).send(helpEmbed);
+        }
+    },
+    {
+        name: "guildMemberRemove",
+        execute(member) {
+            const helpEmbed = new mess.MessageEmbed()
+
+            .setColor(0x28c9d0)
+
+            .setTitle(`${member.user.tag} (${member.id})`)
+
+            .setDescription("Left the Guild!")
+
+            .setFooter("Legacy Audit: Left")
+
+            .setTimestamp();
+            member.guild.channels.cache.find(channel => channel.name === welcome).send(helpEmbed); // u can replace `welcome` as a string, instead of parsing through a json file.
+        }
     }
-  },
-  {
-    name: "guildMemberRemove",
-    execute(member) {
-      const helpEmbed = new mess.MessageEmbed()
-
-        .setColor(0x28c9d0)
-
-        .setTitle(`${member.user.tag} (${member.id})`)
-
-        .setDescription("Left the Guild!")
-
-        .setFooter("Legacy Audit: Left")
-
-        .setTimestamp();
-      member.guild.channels.cache.get("743231991897522256").send(helpEmbed);
-      
-    }
-  }
 ];
